aa67731854e5436bc34ab0ef2ec2a22f09268599
nohaelboghdady <55716798+nohaelboghdady@users.noreply.github.com>
2020-08-18 18:46:35 +0200

    Add files via upload

---
+++ build/precache-manifest.8a681fd4be465dde6773236715db262d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7794771220501d268d41a1c01cbb2d0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b2aa30bbab27d6e48479",
+    "url": "/static/css/main.cf27a347.chunk.css"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/static/js/2.526585eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.526585eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aa30bbab27d6e48479",
+    "url": "/static/js/main.506e9b7e.chunk.js"
+  },
+  {
+    "revision": "df3626599cd0ef421114",
+    "url": "/static/js/runtime-main.75482288.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
